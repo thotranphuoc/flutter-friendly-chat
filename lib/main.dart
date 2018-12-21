6445d54c372d1f61623e5a5ecc481c4d0474e3db
mac <mac@Jade-MBP.net.fpt>
2018-12-21 23:28:25 +0700

    make send button context-aware: highlight when user types something

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -58,6 +59,12 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             new Flexible(
               child: new TextField(
                 controller: _textController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.trim().length > 0;
+                  });
+                  print(_isComposing);
+                },
                 onSubmitted: _handleSubmitted,
                 decoration:
                     new InputDecoration.collapsed(hintText: "Send a message"),
@@ -66,9 +73,10 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             new Container(
               margin: const EdgeInsets.symmetric(horizontal: 4.0),
               child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => _handleSubmitted(_textController.text),
-              ),
+                  icon: new Icon(Icons.send),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null),
             )
           ],
         ),
@@ -85,6 +93,9 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
     ChatMessage message = new ChatMessage(
       text: text,
       animationController: new AnimationController(
