cc5a5f05e406f3e855f23b8d7bbc550903518d4d
mac <mac@Jade-MBP.local>
2018-12-21 19:43:01 +0700

    apply Theme

--- lib/main.dart
+++ lib/main.dart
@@ -41,26 +41,29 @@ class ChatScreenState extends State<ChatScreen> {
   }
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 0.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textController,
-              onSubmitted: _handleSubmitted,
-              decoration:
-                  new InputDecoration.collapsed(hintText: "Send a miessage"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 0.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a miessage"),
+              ),
             ),
-          ),
-          new Container(
-            margin: const EdgeInsets.symmetric(horizontal: 0.0),
-            child: new IconButton(
-              icon: new Icon(Icons.send),
-              onPressed: () => _handleSubmitted(_textController.text),
-            ),
-          )
-        ],
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
